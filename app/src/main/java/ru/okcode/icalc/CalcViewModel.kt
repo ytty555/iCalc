eda38bc465a4dfe2cc68516125a5eaedebbe9e48
Yuriy Titov <yurtitov@gmail.com>
2020-07-06 11:29:37 +0200

    CalcViewModel in progress

--- app/src/main/java/ru/okcode/icalc/CalcViewModel.kt
+++ app/src/main/java/ru/okcode/icalc/CalcViewModel.kt
@@ -1,9 +1,16 @@
 package ru.okcode.icalc
 
 import androidx.lifecycle.ViewModel
+import ru.okcode.icalc.command.Command
 
 class CalcViewModel : ViewModel() {
 
+    fun onClickDigit(digit: Int) {
+        TODO()
+    }
 
+    fun onClickOperator(command: Command) {
+        TODO()
+    }
 
 }
---
+++ app/src/main/java/ru/okcode/icalc/command/Command.kt
@@ -0,0 +1,4 @@
+package ru.okcode.icalc.command
+
+interface Command {
+}
